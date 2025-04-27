83010d8fc72e21b7b5d7d6abb4fe9dc57df15ea2
qi7876 <97651950+qi7876@users.noreply.github.com>
2025-04-27 15:29:31 +0800

    Simplified the project structure.

--- src/detection/main.py
+++ src/detection/main.py
@@ -6,7 +6,6 @@ This module is used to test the LogLLM.
 
 from model import LogLLM
 from database import VectorDatabase
-from get_logs import get_logs
 import tomli
 import time
 
@@ -33,9 +32,30 @@ DATASET_NAME = config["dataset"].get("dataset_name", "")
 DATASET_PATH = config["dataset"].get("dataset_path", "")
 NUM_MAX_LOGS = config["dataset"].get("num_max_logs", 1000)
 
+def get_logs(dataset_name: str, dataset_path: str):
+    logs = []
+    with open(dataset_path, "r") as dataset:
+        for line in dataset:
+            parts = line.split(" ")
+            if dataset_name == "BGL":
+                new_line = " ".join(parts[6:])
+            elif dataset_name == "Thunderbird":
+                new_line = " ".join(parts[7:])
+            elif dataset_name == "liberty2":
+                new_line = " ".join(parts[8:])
+            logs.append(new_line)
+    return logs
+
+def pretty_db_response(response):
+    if response.startswith("- "):
+        response = "This is a normal log: " + response[2:]
+    else:
+        first_space_index = response.find(' ')
+        response = "This is a anomaly log: " + response[first_space_index + 1:]
+    return response
 
 # Load the dataset.
-log_list = get_logs(DATASET_NAME)(DATASET_PATH, NUM_MAX_LOGS)
+log_list = get_logs(DATASET_NAME, DATASET_PATH)
 
 # Create an instance of LogLLM.
 logLLM = LogLLM(
@@ -48,44 +68,32 @@ logLLM = LogLLM(
 # Load the vector database for RAG.
 vectorDatabase = VectorDatabase(db_dir=CHROMA_DB_DIR, collection_name=COLLECTION_NAME)
 
-def pretty_db_response(db_response):
-    if db_response.startswith("- "):
-        db_response = "This is a normal log: " + db_response[2:]
-    else:
-        first_space_index = db_response.find(' ')
-        db_response = "This is a anomaly log: " + db_response[first_space_index + 1:]
-    return db_response
-
-counter = 1
-
-def chat_to_llm(log):
-    global counter
-
-    db_response = vectorDatabase.query(log, n_results=1)
-    db_response = pretty_db_response(db_response)
-
-    if db_response != "":
-        user_input = {"log": log, "db_response": db_response}
-        output = logLLM.chat(PROMPT_WITH_RAG ,user_input)
-    else:
-        user_input = {"log": log}
-        output = logLLM.chat(PROMPT_WITHOUT_RAG, user_input)
-
-    with open("./tools/output.txt", "a") as f:
-        f.write(str(counter) + output + "\n")
-
-    counter += 1
-
 
 if __name__ == "__main__":
     start_time = time.time()
 
     # Start the main loop.
     try:
-        for new_log in log_list:
-            chat_to_llm(new_log)
+        for log in log_list:
+            counter = 1
+            db_response = vectorDatabase.query(log, n_results=1)
+            db_response = pretty_db_response(db_response)
+
+            if db_response != "":
+                user_input = {"log": log, "db_response": db_response}
+                output = logLLM.chat(PROMPT_WITH_RAG, user_input)
+            else:
+                user_input = {"log": log}
+                output = logLLM.chat(PROMPT_WITHOUT_RAG, user_input)
+
+            with open("./tools/output.txt", "a") as f:
+                f.write(str(counter) + output + "\n")
+
+            counter += 1
     except KeyboardInterrupt:
         print("STOP")
+    except Exception as e:
+        print("Unexpected error: ", e)
 
     end_time = time.time()
 
